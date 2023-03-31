f71c18752b2e56078604323c95f3368e5b1c1bdf
Frusan <brunaiscz@gmail.com>
2023-03-31 02:27:41 -0300

    primeiroCommit

---
+++ primeiroProjeto.rb
@@ -0,0 +1,8 @@
+print "Digite o seu nome:"
+nome  = gets.chomp
+print "Digite o seu sobrenome:"
+sobrenome  = gets.chomp
+print "Digite sua idade:"
+idade = gets.chomp
+
+puts "O leitor #{nome} #{sobrenome} possui #{idade} anos"
